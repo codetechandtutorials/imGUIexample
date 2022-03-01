4ed42db9b27fae06df1f587f67d84d03dd334f42
Matthew Early <matthewjearly@gmail.com>
2022-02-28 22:20:33 -0500

    color slider changes background color

--- imGUIexample/imGUIexample.cpp
+++ imGUIexample/imGUIexample.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+
 class CustomImGui : public UseImGui {
 public:
 	virtual void Update() override {
@@ -15,14 +16,13 @@ public:
 		static float f = 0.0f;
 		static int counter = 0;
 
-		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+		ImGui::Begin("CustomImGui Hello, world!");              // Create a window called "Hello, world!" and append into it.
 
 		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
 
 		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
 
-		static float clear_color[3];
-		ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+		bool clear_color_changed = ImGui::ColorEdit3("clear color", (float*)clear_color); // Edit 3 floats representing a color
 
 		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
 			counter++;
@@ -33,6 +33,18 @@ public:
 
 		ImGui::End();
 
+
+
+		if (clear_color_changed) {
+			change_clear_color(clear_color[0], clear_color[1], clear_color[2]);
+		}
+
+	}
+
+private:
+	float clear_color[3] = { .0f, .0f, .0f };
+	void change_clear_color(float r, float g, float b) {
+		glClearColor(r, g, b, 1.00f);
 	}
 };
 
@@ -69,7 +81,7 @@ int main()
 	myimgui.Init(window, glsl_version);
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
-		glClearColor(0.45f, 0.55f, 0.60f, 1.00f);
+
 
 
 		glClear(GL_COLOR_BUFFER_BIT);
